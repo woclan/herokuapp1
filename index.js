0619a64db97afae24e3741e771d82e0c1d35cc64
Luis Hernandez <woclan@gmail.com>
2018-07-20 21:02:55 -0500

    LH: Add more changes

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+console.log('hola');
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const MongoClient = require('mongodb').MongoClient
+var db;
+
+
+MongoClient.connect('mongodb://luis:luis01@ds141671.mlab.com:41671/luis', (err, database) => {
+	if(err)
+        throw err;
+    console.log("connected to the mongoDB !");
+    db=database.db('luis');
+// ... start the server
+})
+
+
+const express = require('express');
+const bodyParser= require('body-parser')
+
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.listen(3000, function() {
+console.log('listening on 3000')
+})
+
+
+app.post('/', function(req, res) {
+res.sendFile('/Users/luism/expressproj/index.html')
+
+
+})
+
+
+app.get('/quotes', (req, res) => {
+console.log('Hellooooooooooooooooo!');
+db.collection("names").find({}).toArray(function(err, data) {
+         res.send(data);
+     });
+
+
+console.log(req.body)
+
+})
+
